e5df8736d6dec1e5d4d1f157e20afa0ffb2c7bbd
laerer5cmysen <99287788+laerer5cmysen@users.noreply.github.com>
2022-02-08 20:51:07 +0100

    Startfiler for MakeCode-prosjektet

---
+++ main.ts
@@ -0,0 +1,36 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 0) {
+        basic.showNumber(0)
+        bitbot.setLedColor(0x80FF00)
+        bitbot.goms(BBDirection.Forward, 60, 400)
+    } else if (receivedNumber == 1) {
+        bitbot.setLedColor(0x00FFFF)
+        basic.showNumber(1)
+        bitbot.goms(BBDirection.Forward, 60, 400)
+    } else if (receivedNumber == 2) {
+        bitbot.setLedColor(0x0000FF)
+        basic.showNumber(2)
+        bitbot.rotatems(BBRobotDirection.Right, 60, 400)
+    } else if (receivedNumber == 3) {
+        bitbot.setLedColor(0xFF00FF)
+        basic.showNumber(3)
+        bitbot.rotatems(BBRobotDirection.Left, 60, 400)
+    } else if (receivedNumber == 4) {
+        bitbot.setLedColor(0xFFFFFF)
+        basic.showNumber(4)
+        bitbot.bbSetServo(BBServos.P1, 60)
+        bitbot.bbSetServo(BBServos.P2, 120)
+    } else if (receivedNumber == 5) {
+        bitbot.setLedColor(0xFFFF00)
+        basic.showNumber(5)
+        bitbot.bbSetServo(BBServos.P1, 120)
+        bitbot.bbSetServo(BBServos.P2, 60)
+    } else if (receivedNumber == 6) {
+        bitbot.setLedColor(0x00FF00)
+        basic.showNumber(6)
+        bitbot.bbSetServo(BBServos.P1, 90)
+        bitbot.bbSetServo(BBServos.P1, 90)
+    }
+})
+radio.setGroup(1)
+bitbot.select_model(BBModel.XL)
