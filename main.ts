27699010a7f7af3056a04591769fce1e142cd19b
laerer5cmysen <99287788+laerer5cmysen@users.noreply.github.com>
2022-02-09 08:30:32 +0100

    Oppdater main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,19 +2,19 @@ radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 0) {
         basic.showNumber(0)
         bitbot.setLedColor(0x80FF00)
-        bitbot.goms(BBDirection.Forward, 60, 400)
+        bitbot.goms(BBDirection.Forward, 60, 500)
     } else if (receivedNumber == 1) {
         bitbot.setLedColor(0x00FFFF)
         basic.showNumber(1)
-        bitbot.goms(BBDirection.Forward, 60, 400)
+        bitbot.goms(BBDirection.Reverse, 60, 500)
     } else if (receivedNumber == 2) {
         bitbot.setLedColor(0x0000FF)
         basic.showNumber(2)
-        bitbot.rotatems(BBRobotDirection.Right, 60, 400)
+        bitbot.rotatems(BBRobotDirection.Right, 30, 400)
     } else if (receivedNumber == 3) {
         bitbot.setLedColor(0xFF00FF)
         basic.showNumber(3)
-        bitbot.rotatems(BBRobotDirection.Left, 60, 400)
+        bitbot.rotatems(BBRobotDirection.Left, 30, 400)
     } else if (receivedNumber == 4) {
         bitbot.setLedColor(0xFFFFFF)
         basic.showNumber(4)
